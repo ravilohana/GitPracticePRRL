79e0ecec74d955817d786d9a7b55c21daba724f2
ravilohana <ravi.lohana1987@gmail.com>
2025-08-20 12:43:27 +0530

    Initial commit

---
+++ tests/my-test.spec.ts
@@ -0,0 +1,35 @@
+import {expect, test} from '@playwright/test'
+
+
+test('Google Test', async ({ page }) => {
+    await page.goto("https://www.google.com");
+    console.log("Google Title: ", await page.title());
+    console.log("Google URL: ",page.url());
+    expect(await page.title()).toEqual("Google");
+})
+
+
+test('Facebook Test', async ({ page }) => {
+    await page.goto("https://www.facebook.com/");
+    console.log("Facebook Title: ", await page.title());
+    console.log("Facebook URL: ",page.url());
+    expect(await page.title()).toEqual("Facebook – log in or sign up");
+})
+
+
+
+test('X Test', async ({ page }) => {
+    await page.goto("https://www.x.com");
+    console.log("X Title: ", await page.title());
+    console.log("X URL: ",page.url());
+    expect(await page.title()).toEqual("X. It’s what’s happening / X");
+})
+
+
+
+test('Youtube Test', async ({ page }) => {
+    await page.goto("https://www.youtube.com");
+    console.log("Youtube Title: ", await page.title());
+    console.log("Youtube URL: ",page.url());
+    expect(await page.title()).toEqual("YouTube");
+})
